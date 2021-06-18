cb9fcc08fe9b8d6e196f2849a5bf78134e1e328b
Henrymanimus <vuhieu24042000@gmail.com>
2021-06-18 17:25:49 +0700

    task1_hieu_design footer.blade.php

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,11 +1,14 @@
- <!-- footer -->
- <footer>
+    <!-- footer -->
+    <footer>
         <div class="footer">
            <div class="container">
               <div class="row pdn-top-30">
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                 <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
                     <div class="Follow">
                        <h3>Follow Us</h3>
+                    </div>
+                    <div><a href="##"><img src="images/logo.png"></a>
+                        <p></p>
                     </div>
                     <ul class="location_icon">
                        <li> <a href="#"><img src="icon/facebook.png"></a></li>
@@ -14,37 +17,51 @@
                        <li> <a href="#"><img src="icon/instagram.png"></a></li>
                     </ul>
                  </div>
-               <!--Introduce Footer-->
-                  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 item">
+                 
+                <!--Short Link-->
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 item">
+                        <div class="Follow">
+                           <h3>MEMORIAL</h3>
+                        </div>
+                     <div class="footer-shortlink">
+                        <ul>
+                           <li><a href="#">Home</a></li>
+                           <li><a href="#">About Us</a></li>
+                           <li><a href="#">Contact</a></li>
+                        </ul>
+                     </div>
+                  </div>
+
+                <!--Introduce Footer-->
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 item">
                      <div class="Follow">
                         <h3>Find us</h3>
                      </div>
                      <div style="color: white">
-                        <p>Tâm hồn bạn đen tối lắm rồi, bạn cần được thanh tẩy, thế nên hãy tìm đến sách</p></br>
-                        <p><i class="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35 </p>
-                        <p><i class="fa fa-phone"></i>  +91-9999878398  </p>
-                        <p><i class="fa fa fa-envelope"></i> info@example.com  </p>  
+                        <p style="font-size: small"> Memorial bring the bes experience for you, The cosy atmosphere here is
+                           wating for you
+                        </p></br>
+                        <div class="footer-shortlink">
+                           <li><a target="blank" href="https://goo.gl/maps/cUPcLhpbbMfWtdgHA"><i class="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35 </a></li>
+                           <p><i class="fa fa-phone"></i>  +91-9999878398  </p>
+                           <li><a href="mailto:support@harvelelectric.com"><i class="fa fa fa-envelope"></i> info@example.com</a> </li>
+                        </div>
                      </div>     
                  </div>
-               
-               <!--Short Link-->
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 item">
-                     <div class="Follow">
-                        <h3>MEMORIAL</h3>
+
+                <!--Google map-->
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 item">
+                     <div id="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26360649.057984937!2d-113.7437232213936!3d36.24307279902737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2zSG9hIEvhu7M!5e0!3m2!1svi!2s!4v1623930327039!5m2!1svi!2s" width="380" height="210" style="border:1;" allowfullscreen=""></iframe>
                      </div>
-                        <ul class="footer-shortlink">
-                        <li><a  href="#">Home</a></li>
-                        <li><a  href="#">About Us</a></li>
-                        <li><a  href="#">Contact</a></li>
-                        </ul>
-                 </div>
+                  </div>
+
               </div>
            </div>
         </div>
         <div class="copyright">
            <div class="container">
-              <p>Copyright 2019 All Right Reserved By <a href="https://html.design/">Free html Templates</a></p>
+              <p>Copyright 2019 All Right Reserved By <a target="blank" href="https://www.facebook.com/phuong.nguyen.351756">TEAM4Phuong</a></p>
            </div>
         </div>
      </footer>
-     <!-- end footer -->
