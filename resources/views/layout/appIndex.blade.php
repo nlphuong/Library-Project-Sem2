a65c44c52a8f3ca64c55dc784a45774b616f0f54
nlphuong <nlphuong95@gmail.com>
2021-06-19 15:40:06 +0700

    task#00 fixed conflict.

--- resources/views/layout/appIndex.blade.php
+++ resources/views/layout/appIndex.blade.php
@@ -14,7 +14,6 @@
       <meta name="author" content="">
       <!-- bootstrap css -->
       <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-
       <!-- style css -->
       <link rel="stylesheet" href="{{asset('css/style.css')}}">
       <!-- Responsive-->
@@ -30,101 +29,14 @@
    </head>
    <!-- body -->
    <body class="@yield('body-class')">
-      <!-- loader  -->
-      <div class="loader_bg">
-         <div class="loader"><img src="{{asset('images/loading.gif')}}" alt="#" /></div>
-      </div>
-      <!-- end loader -->
-      <!-- header -->
-      <header>
-         <!-- header inner -->
-         <div class="header">
-            <div class="container">
-               <div class="row">
-                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-                     <div class="full">
-                        <div class="center-desk">
-                           <div class="logo"> <a href="{{url('/')}}"><img src="{{asset('images')}}/logo.png" alt="#"></a> </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                     <div class="menu-area">
-                        <div class="limit-box">
-                           <nav class="main-menu">
-                              <ul class="menu-area-main">
-                                 <li> <a href="{{url('/')}}">Home</a> </li>
-                                 <li> <a href="{{url('about')}}">About us</a> </li>
-                                 <li><a href="{{url('books')}}">Our Books</a></li>
-                                 <li><a href="{{url('library')}}">library</a></li>
-                                 <li><a href="{{url('contact')}}">Contact us</a></li>
-                                 <li> <a href="#"><img src="{{asset('images/search_icon.png')}}" alt="#" /></a> </li>
 
+      @include('common.loader')
+      @include('common.header')
 
-                                    @if(session('accountSession'))
-                                    <li>
-                                        <a href="#" class="drop-menu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">hi {{session('accountSession')[0]['fullname']}}<i class="fa fa-caret-down" aria-hidden="true"></i></a>
-                                        <div class="dropdown-menu dropdown-menu-right">
-                                            <a href="#" class="dropdown-item drop-content" >Profile</a>
-                                            <a href="{{url('/logout')}}" class="dropdown-item drop-content" >Logout</a>
+         @yield('main')
+         
+      @include('common.footer')   
 
-                                          </div>
-
-                                    </li>
-                                    @endif
-                                    @if(session('adminSession'))
-                                        <li><a href="{{url('admin/index')}}"><img src="{{asset('images')}}/admin.png" width="30px" alt=""></a></li>
-
-                                    @endif
-
-
-
-                                 <li></li>
-                                </ul>
-                           </nav>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-
-         <!-- end header inner -->
-      </header>
-
-      @yield('main')
-      <footer>
-        <div class="footer">
-           <div class="container">
-              <div class="row pdn-top-30">
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                    <div class="Follow">
-                       <h3>Follow Us</h3>
-                    </div>
-                    <ul class="location_icon">
-                       <li> <a href="#"><img src="{{asset('icon')}}/facebook.png"></a></li>
-                       <li> <a href="#"><img src="{{asset('icon')}}/Twitter.png"></a></li>
-                       <li> <a href="#"><img src="{{asset('icon')}}/linkedin.png"></a></li>
-                       <li> <a href="#"><img src="{{asset('icon')}}/instagram.png"></a></li>
-                    </ul>
-                 </div>
-                 <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
-                    <div class="Follow">
-                       <h3>Newsletter</h3>
-                    </div>
-                    <input class="Newsletter" placeholder="Enter your email" type="Enter your email">
-                    <button class="Subscribe">Subscribe</button>
-                 </div>
-              </div>
-           </div>
-        </div>
-        <div class="copyright">
-           <div class="container">
-              <p>Copyright 2019 All Right Reserved By <a href="https://html.design/">Free html Templates</a></p>
-           </div>
-        </div>
-     </footer>
-     <!-- end footer -->
      <!-- Javascript files-->
      @if(Session::has('login'))
         <script>
@@ -139,6 +51,5 @@
      <!-- sidebar -->
      <script src="{{asset('js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
      <script src="{{asset('js/custom.js')}}"></script>
-
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 // Route không áp dụng middleware
 Route::get('/','UserController@index');
+Route::get('/books','BookController@showAllBook');
 Route::get('/register','UserController@register');
 Route::post('/register','UserController@postRegister');
 Route::post('/postLogin','UserController@postLogin');
