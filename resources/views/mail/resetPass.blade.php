ece75315a010c44feaea03c819331999696f610f
sangtrancong <sangtrancong171196@gmail.com>
2021-06-26 10:52:21 +0700

    Task#sang#resetPass

---
+++ resources/views/mail/resetPass.blade.php
@@ -0,0 +1,5 @@
+<h4>Dear {{$data['name']}}</h4>
+<p>Memorial Library are very happy to see you again!</p>
+<p>New Password of your account:  <strong style="color: red">{{$data['password']}}</strong></p>
+<p><i>If you have any questions please send  email to memoriallibrary123@gmail.com!</i></p>
+<p>Thank you!</p>
